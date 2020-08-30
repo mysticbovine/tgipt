fc37823751cbd8c8238f388a64692d88952ac7fe
Rob Evans <rob@rockycoastcreative.ca>
2020-08-30 14:12:02 -0700

    fixed untracked files

--- sites/all/themes/tgipt/templates/page.tpl.php
+++ sites/all/themes/tgipt/templates/page.tpl.php
@@ -133,8 +133,24 @@
 								echo "<li><a href='/user'>Log in </a></li>  <li><a href='/user/register'>Sign up</a></li> ";	
 							}?>
 							<li><a href="tel:+16046893647"><span class="phonenumber">Phone us: 1-604-882-7505</span></a></li> 
-							<li><a href="/our-distributors">Our Distrubutors</a></li> 
-							<li><a href="/retailers-carrying-our-treats">Our Retailers</a></li> 
+							<li class="last expanded dropdown hidden"><a href="/contact" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Contact <span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li class="first leaf"><a href="/contact" title="">Contact</a></li>
+									<li class="last leaf"><a href="/charity-community-support">Charity &amp; Community</a></li>
+								</ul>
+							</li>
+							<li><a href="/blog">Blog</a></li> 
+							<li><a href="/our-distributors">Our Distributors</a></li> 
+							<li><a href="/retailers-carrying-our-treats">Our Retailers</a></li>
+							<li class="first expanded dropdown hidden"><a href="/why-granville-island-pet-treatery" class="dropdown-toggle" data-toggle="dropdown">Why Us? <span class="caret"></span></a>
+								<ul class="dropdown-menu"><li class="first leaf"><a href="/why-granville-island-pet-treatery" title="">Why Pet Treatery?</a></li>
+									<li class="leaf"><a href="/why-pet-treatery/our-vision-mission-values">Our Vision, Mission and Values</a></li>
+									<li class="leaf"><a href="/why-pet-treatery/recipes-and-ingredients">Recipes and Ingredients</a></li>
+									<li class="leaf"><a href="/why-pet-treatery/caboose">The Caboose</a></li>
+									<li class="leaf"><a href="/about/testimonials" title="">Testimonials</a></li>
+									<li class="last leaf"><a href="/why-tgipt/faq">FAQ</a></li>
+								</ul>
+							</li>
 							<li><a href="/"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li> 
 					
 					
@@ -276,3 +292,4 @@
 
   </footer>
 <?php endif; ?>
+v

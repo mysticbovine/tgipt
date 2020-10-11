179b4bdeb98306b238cd7861c1a811d0beb332b4
Rob Evans <rob@rockycoastcreative.ca>
2020-10-10 20:13:49 -0700

    Mega Menu styled for mobile.

--- sites/all/themes/tgipt/templates/page.tpl.php
+++ sites/all/themes/tgipt/templates/page.tpl.php
@@ -77,15 +77,15 @@
 ?>
 <header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
 	<div class="<?php print $container_class; ?> row">
- <!-- Mobile Header -->
- <div class="wsmobileheader clearfix">
-				<a id="wsnavtoggle" class="wsanimated-arrow"><span></span></a>
-				<span class="smllogo">
-					<a class="" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-						<img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="img-responsive"/>
-					</a>
-				</span>
-				<div class="wssearch clearfix">
+		<!-- Mobile Header -->
+		<div class="wsmobileheader clearfix">
+			<a id="wsnavtoggle" class="wsanimated-arrow"><span></span></a>
+			<span class="smllogo">
+				<a class="" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+					<img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="img-responsive"/>
+				</a>
+			</span>
+			<div class="wssearch clearfix hidden">
 				<i class="wsopensearch fas fa-search"></i>
 				<i class="wsclosesearch fas fa-times"></i>
 				<div class="wssearchform clearfix">
@@ -93,37 +93,20 @@
 					<input type="text" placeholder="Search Here">
 					</form>
 				</div>
-				</div>
 			</div>
-  			<!-- Mobile Header -->
-	  <div class="logo col-sm-2">
-	  	<div class="navbar-header">
-		  <?php if ($logo): ?>
-			<a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-			  <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="img-responsive"/>
-			</a>
-		  <?php endif; ?>
-
-		  <?php if (!empty($site_name)): ?>
-			<a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
-		  <?php endif; ?>
-
-		  <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-			  <span class="sr-only"><?php print t('Toggle navigation'); ?></span>
-			  <span class="menu-hint"><?php print t('Menu'); ?></span>
-			  <span class="icon-bar"></span>
-			  <span class="icon-bar"></span>
-			  <span class="icon-bar"></span>
-			</button>
-		  <?php endif; ?>
-		   
 		</div>
-	  
-	  </div>
+		<!-- Mobile Header -->
+		<div class="logo col-sm-2">
+			<div class="navbar-header">
+				<?php if ($logo): ?>
+					<a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+						<img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="img-responsive"/>
+					</a>
+				<?php endif; ?>
+			</div>
+		</div>
 
-	  <div class="menu col-sm-10">
-	
+	<div class="menu col-sm-10">
 		<div class="navbar-collapse0 collapse0">
 			<?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
 			<div class="row">
@@ -141,26 +124,25 @@
 							} else {
 								echo "<li><a href='/user'>Log in </a></li>  <li><a href='/user/register'>Sign up</a></li> ";	
 						}?>
-							 
-						
-							 
-					
-							<li><a href="/cart">Cart: 
-									<?php  
-										if(!isset($cid)) {$cid = NULL;}
-										$item = uc_cart_get_total_qty($cid);
-										if($item == 0) {
-											echo " 0 items";
-										} else {										
-											if ($item == 1) {												
-												echo " $item item";
-											} else {
-												echo "$item items";
-											} 
-										} 
-									?>
-								</a>
-							</li>
+
+					<li>
+						<a href="/cart">Cart: 
+							<?php 
+							// Set up as a function?
+								if(!isset($cid)) {$cid = NULL;}
+								$item = uc_cart_get_total_qty($cid);
+								if($item == 0) {
+									echo " 0 items";
+								} else {										
+									if ($item == 1) {												
+										echo " $item item";
+									} else {
+										echo "$item items";
+									} 
+								} 
+							?>
+						</a>
+					</li>
 				</div>
 			</div>
 			
@@ -187,44 +169,35 @@
 			</div>
 				<!-- end -->
 
-			 <div class="row mega">
+			<div class="row mega">
 				<nav role="navigation" class="mainmenu wsmenu">
-				<ul class="wsmenu-list">
-					<?php print tgipt_megamenu("dog");?>
-					<?php print tgipt_megamenu("cat");?>
-				  <?php if (!empty($primary_nav)): ?>
-				  
-					<?php print render($primary_nav); ?>
-				  <?php endif; ?>
-				  <?php if (!empty($secondary_nav)): ?>
-					<?php print render($secondary_nav); ?>
-				  <?php endif; ?>
-				  <?php if (!empty($page['navigation'])): ?>
-					<?php print render($page['navigation']); ?>
-				  <?php endif; ?>
-				  <!-- Mega Menu function test -->
-				</ul>
-				
-		  		
-         
-				
-					  
+					<ul class="wsmenu-list">
+						<?php print tgipt_megamenu("dog");?>
+						<?php print tgipt_megamenu("cat");?>
+					<?php if (!empty($primary_nav)): ?>
+					
+						<?php print render($primary_nav); ?>
+					<?php endif; ?>
+					<?php if (!empty($secondary_nav)): ?>
+						<?php print render($secondary_nav); ?>
+					<?php endif; ?>
+					<?php if (!empty($page['navigation'])): ?>
+						<?php print render($page['navigation']); ?>
+					<?php endif; ?>
+					<!-- Mega Menu function test -->
+					<!-- Add account items here. -->
+					</ul>
 				</nav>
-			</div>
+			</div> <!-- end mega row-->
 			<?php endif; ?>
-		</div>
-  
-	  
-	  </div>
-
-
-  </div>
+		</div><!-- end old navbar collapse bar NOT NEEDED -->
+	  </div> <!-- end menu row-->
+  	</div> <!-- ??-->
 </header>
+
 <div class="main-site">
-	<?php if (!empty($page['highlighted'])): ?>
-		
-			<?php print render($page['highlighted']); ?>
-	
+	<?php if (!empty($page['highlighted'])): ?>	
+		<?php print render($page['highlighted']); ?>
 	<?php endif; ?>
 	<?php if (!empty($breadcrumb)): ?>
 		<div class="container-fluid breadcrumb-fluid">
@@ -233,60 +206,60 @@
 			</div>
 		</div>
 	<?php endif;?>
-<div class="main-container container">
+	<div class="main-container container">
 
-  <header role="banner" id="page-header">
-    <?php if (!empty($site_slogan)): ?>
-      <p class="lead"><?php print $site_slogan; ?></p>
-    <?php endif; ?>
- <?php print render($page['header']); ?>
-  
-  </header> <!-- /#page-header -->
+		<header role="banner" id="page-header">
+				<?php if (!empty($site_slogan)): ?>
+				<p class="lead"><?php print $site_slogan; ?></p>
+				<?php endif; ?>
+				<?php print render($page['header']); ?>	 
+		</header> <!-- /#page-header -->
 
-  <div class="row">
+		<div class="row">
+			<?php if (!empty($page['sidebar_first'])): ?>
+			<aside class="col-sm-3" role="complementary">
+				<?php print render($page['sidebar_first']); ?>
+			</aside>  <!-- /#sidebar-first -->
+			<?php endif; ?>
 
-    <?php if (!empty($page['sidebar_first'])): ?>
-      <aside class="col-sm-3" role="complementary">
-        <?php print render($page['sidebar_first']); ?>
-      </aside>  <!-- /#sidebar-first -->
-    <?php endif; ?>
+			<section<?php print $content_column_class; ?>>
+			
+			
+			<a id="main-content"></a>
+			<?php print render($title_prefix); ?>
+			<?php if (!empty($title)): ?>
+				<h1 class="page-header"><?php print $title; ?></h1>
+			<?php endif; ?>
+			<?php print render($title_suffix); ?>
+			<?php print $messages; ?>
+			<?php if (!empty($tabs)): ?>
+				<?php print render($tabs); ?>
+			<?php endif; ?>
+			<?php if (!empty($page['help'])): ?>
+				<?php print render($page['help']); ?>
+			<?php endif; ?>
+			<?php if (!empty($action_links)): ?>
+				<ul class="action-links"><?php print render($action_links); ?></ul>
+			<?php endif; ?>
+			<?php print render($page['content']); ?>
+			</section>
 
-    <section<?php print $content_column_class; ?>>
-    
-      
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if (!empty($title)): ?>
-        <h1 class="page-header"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php if (!empty($tabs)): ?>
-        <?php print render($tabs); ?>
-      <?php endif; ?>
-      <?php if (!empty($page['help'])): ?>
-        <?php print render($page['help']); ?>
-      <?php endif; ?>
-      <?php if (!empty($action_links)): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-    </section>
+			<?php if (!empty($page['sidebar_second'])): ?>
+			<aside class="col-sm-3" role="complementary">
+				<?php print render($page['sidebar_second']); ?>
+			</aside>  <!-- /#sidebar-second -->
+			<?php endif; ?>
 
-    <?php if (!empty($page['sidebar_second'])): ?>
-      <aside class="col-sm-3" role="complementary">
-        <?php print render($page['sidebar_second']); ?>
-      </aside>  <!-- /#sidebar-second -->
-    <?php endif; ?>
-
-  </div>
+		</div>
+	</div>
 </div>
-</div>
+
 <?php if (!empty($page['abovefooter'])): ?>
 	<div class="container-fluid abovefooter">
 		<?php print render($page['abovefooter']); ?>
 	</div>
 <?php endif; ?>
+
 <div class="container teaser">
 	<div class="row">
 		<div class="col-sm-4">
@@ -300,6 +273,7 @@
 		</div>
 	</div>
 </div>
+
 
 <?php if (!empty($page['footer'])): ?>
   <footer class="footer <?php print $container_class; ?>">
